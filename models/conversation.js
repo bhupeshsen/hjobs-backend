40cc27a5e66995183d9cb623a0befc1960df16c1
bhupeshsen <bhupeshsen11@gmail.com>
2021-05-29 16:00:23 +0530

    upate api

--- models/conversation.js
+++ models/conversation.js
@@ -2,29 +2,14 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
-  to: {
-    userId: { type: Schema.Types.ObjectId },
-    userType: String
-  },
-  from: {
-    userId: Schema.ObjectId,
-    userType: String
-  },
-  message: {
-    _text: {
-      type: String,
-      trim: true
-    },
-    _type: {
-      type: String,
-      trim: true,
-      default: 'text'
-    }
-  },
+  to: { type: Schema.Types.ObjectId, ref: 'User' },
+  from: { type: Schema.Types.ObjectId, ref: 'User' },
+  message: { type: String, trim: true },
   createdAt: {
     type: Date,
     default: Date.now()
   },
 });
 
-module.exports = mongoose.model('Conversation', schema);
+const Conversation = mongoose.model('Conversation', schema);
+module.exports = { Conversation };
